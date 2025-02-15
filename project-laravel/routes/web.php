6804daa603ad7a447f00e454296fc22e9efa9d34
micheliacampaka <micheliacmpa@gmail.com>
2025-02-15 20:53:03 +0700

    Update web.php

--- project-laravel/routes/web.php
+++ project-laravel/routes/web.php
@@ -1,29 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ManagementUserController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-route::get('/foo', function (){
-    return 'Hello Word';
-});
-route::get('user/{id}', function($id) {
-    return 'user'.$id;
-});
-Route::get('/user', [UserController::class, 'index']);
-
-//Route::get($uri, $callback);
-//Route::post($uri, $callback);
-//Route::put($uri, $callback);
-//Route::patch($uri, $callback);
-//Route::delete($uri, $callback);
-//Route::options($uri, $callback);
-
-Route::redirect('/coba','/sini');
-Route::get('/profile', function(){
-    return view('profile',[
-        'nama' => 'Hesti',
-        'nim'=> 'e41232454'
-    ]);
-});
+Route::get('/user', [ManagementUserController::class, 'index']);
+Route::get('/user/create', [ManagementUserController::class, 'create']);
+Route::post('/user', [ManagementUserController::class, 'store']);
+Route::get('/user/{id}', [ManagementUserController::class, 'show']);
+Route::get('/user/{id}/edit', [ManagementUserController::class, 'edit']);
+Route::put('/user/{id}', [ManagementUserController::class, 'update']);
+Route::delete('/user/{id}', [ManagementUserController::class, 'destroy']);
